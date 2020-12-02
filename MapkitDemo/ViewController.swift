f2908805a394b24730ab0edadc136f3183c41338
weibinw219 <weibinw0219@gmail.com>
2020-12-02 14:49:24 -0500

    Initial Commit

---
+++ MapkitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapkitDemo
+//
+//  Created by Weibin Wang on 2020-12-02.
+//  Copyright © 2020 Weibin Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
