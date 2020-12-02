20131c5494022ff0872a6764bc49729684b98920
weibinw219 <weibinw0219@gmail.com>
2020-12-02 15:13:19 -0500

    Create folders and necessary frameworks

---
+++ MapkitDemo/View Controllers/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  MapkitDemo
+//
+//  Created by Weibin Wang on 2020-12-02.
+//  Copyright © 2020 Weibin Wang. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
